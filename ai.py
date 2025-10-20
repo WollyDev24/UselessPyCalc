95c7c23816b479b8553bb3387f2aafbac23e6841
WollyDev24 <225042034+WollyDev24@users.noreply.github.com.>
2025-10-20 19:46:03 +0200

    Added ai chat bot and progress bar

---
+++ ai.py
@@ -0,0 +1,12 @@
+# UselessPyAi - A simple ai chatbot that does nothing useful
+import time
+
+print("Ai V1.0")
+print("Created by WollyDev24")
+print("Enter your Question")
+input1 = input(">> ")
+print("[INFO] Analyzing your Question...")
+time.sleep(2.5)
+print("[INFO] Generating Answer...")
+time.sleep(1.5)
+print("[ANSWER] 42")
