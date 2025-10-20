c9446fb00fd56ad35f754ddb528cd0aab510cd24
WollyDev24 <225042034+WollyDev24@users.noreply.github.com.>
2025-10-20 19:26:32 +0200

    Rename main.py to calc.py

---
+++ calc.py
@@ -0,0 +1,13 @@
+# UselessPyCalc - A simple calculator that does nothing useful
+
+print("Calculator V1.0")
+print("Created by WollyDev24")
+print("Enter first number")
+input1 = input(">> ")
+print("Enter second number")
+input2 = input(">> ")
+print("Enter Third number")
+input3 = input(">> ")
+print("Enter operation (+, -, *, /)")
+operation = input(">> ")
+print("Hello World")
