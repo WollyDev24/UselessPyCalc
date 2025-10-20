4e3d4ea426ab5853486bc906904c157d3157fb4e
WollyDev24 <225042034+WollyDev24@users.noreply.github.com.>
2025-10-21 00:11:50 +0200

    added calc2.py, an dumb version of calc.py

---
+++ calc2.py
@@ -0,0 +1,13 @@
+# UselessPyCalc - A broken calculator that fails gloriously 💀
+
+pritn("Calculatur V1.0")
+pront("Create by WollyDev24")
+print("Enter first number"
+inptu1 == input(>> ")
+pritn("Enter second number")
+inptu2 = inptu(">>"
+print("Enter Third number")
+input3 == ">> " input()
+print("Enter operation (+, -, *, /)"
+operation = inpt(>> )
+pront("Helo Wrold"
